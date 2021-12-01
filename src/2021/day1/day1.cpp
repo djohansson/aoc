8219539d709205e2d4ac1ae3509c2011a83e5fb8
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-01 10:29:17 +0100

    day1 part 1 & 2

--- src/2021/day1/day1.cpp
+++ src/2021/day1/day1.cpp
@@ -1,10 +1,47 @@
+#include <fstream>
 #include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
 
 int main()
 {
     using namespace std;
     
-    cout << "day1\n";
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open())
+        return -1;
+
+    vector<int> numbers;
+    string line;
+    while (getline(inputFile, line))
+        numbers.emplace_back(stoi(line));
+
+    unsigned increasedCount = 0;
+    for (unsigned numberIt = 1; numberIt < numbers.size(); numberIt++)
+    {
+        auto prevNumber = numbers[numberIt-1];
+        auto number = numbers[numberIt];
+        if (prevNumber < number)
+            ++increasedCount;
+    }
+
+    cout << "part 1 increasedCount: " << increasedCount << "\n";
+
+    increasedCount = 0;
+    for (unsigned numberIt = 3; numberIt < numbers.size(); numberIt++)
+    {
+        int A[3], B[3];
+        A[0] = numbers[numberIt-3];
+        A[1] = B[0] = numbers[numberIt-2];
+        A[2] = B[1] = numbers[numberIt-1];
+        B[2] = numbers[numberIt];
+
+        if (accumulate(begin(A), end(A), 0) < accumulate(begin(B), end(B), 0))
+            ++increasedCount;
+    }
+
+    cout << "part 2 increasedCount: " << increasedCount << "\n";
     
     return 0;
 }
