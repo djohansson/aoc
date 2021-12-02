7a09209829128685eba4de29bb69b4d60b456aa0
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-02 20:42:41 +0100

    day8 part2

--- src/2020/day8/day8.cpp
+++ src/2020/day8/day8.cpp
@@ -14,13 +14,16 @@ int main()
         return -1;
 
     enum class Op { nop, acc, jmp, };
-    using Instruction = tuple<Op, int, bool>;
-    vector<Instruction> program;
+    using Instruction = tuple<Op, int>;
+    using Program = vector<Instruction>;
+    using State = tuple<unsigned, int, vector<bool>>; //pc, acc, executed
 
+    Program program;
     string line;
     while (getline(inputFile, line, '\n'))
     {
-        auto& [op, value, executed] = program.emplace_back();
+        auto& [op, value] = program.emplace_back();
+
         if (line.find("nop") == 0)
         {
             op = Op::nop;
@@ -38,36 +41,88 @@ int main()
         }
     }
 
-    unsigned pc = 0;
-    int acc = 0;
-    while (pc < program.size())
+    auto execute = [](const Program& program, State& state)
     {
-        auto& [op, value, executed] = program[pc];
+        auto& [pc, acc, executed] = state;
 
-        if (executed)
-            break;
+        executed.resize(program.size());
+        
+        while (pc < program.size())
+        {
+            const auto& [op, value] = program[pc];
 
-        executed = true;
+            if (executed[pc])
+                return false;
+
+            executed[pc] = true;
+
+            switch (op)
+            {
+            case Op::nop:
+                break;
+            case Op::acc:
+                acc += value;
+                break;
+            case Op::jmp:
+                pc += value;
+                continue;
+            default:
+                break;
+            }
+
+            ++pc;
+        }
+
+        return true;
+    };
+
+    // part 1
+
+    {
+        State state;
+        bool result = execute(program, state);
+        auto& [pc, acc, executed] = state;
+        
+        cout << "part1 result: " << result << "\n";
+        cout << "pc: " << pc << "\n";
+        cout << "acc: " << acc << "\n";
+    }
+
+    // part 2
+
+    State state;
+    for (int i = program.size() - 1; i >= 0; --i)
+    {
+        Program program2 = program;
+
+        auto& [op, value] = program2[i];
 
         switch (op)
         {
         case Op::nop:
-            break;
-        case Op::acc:
-            acc += value;
+            op = Op::jmp;
             break;
         case Op::jmp:
-            pc += value;
-            continue;
+            op = Op::nop;
+            break;
         default:
             break;
         }
 
-        ++pc;
+        State state;
+        bool result = execute(program2, state);
+        auto& [pc, acc, executed] = state;
+
+        if (result)
+        {
+            cout << "part2 result: " << result << "\n";
+            cout << "pc: " << pc << "\n";
+            cout << "acc: " << acc << "\n";
+
+            return 0;
+        }
     }
 
-    cout << acc << "\n";
-
-    return 0;
+    return -1;
 }
 
