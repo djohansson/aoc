a76663bc9c7cf06c4b26516f81646c70fc3fac63
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-03 11:32:53 +0100

    updated day3 part1

--- src/2021/day3/day3.cpp
+++ src/2021/day3/day3.cpp
@@ -11,20 +11,22 @@ int main()
     if (!inputFile.is_open())
         return -1;
 
-    unsigned gamma = 0, epsilon = 0, rowCount = 0;
+    unsigned gamma = 0, epsilon = 0;
+
+    vector<unsigned> numbers;
     vector<unsigned> colBitCounts;
     string line;
     while (getline(inputFile, line, '\n'))
     {
-        ++rowCount;
         colBitCounts.resize(max(colBitCounts.size(), line.size()));
 
+        auto& number = numbers.emplace_back();
         for (unsigned bitIt = 0; bitIt < line.size(); bitIt++)
         {
             switch (line[bitIt])
             {
             case '1':
-                colBitCounts[bitIt]++;
+                number |= 1 << (line.size() - bitIt - 1);
                 break;
             default:
                 break;
@@ -32,11 +34,12 @@ int main()
         }
     }
 
+    for (unsigned numberIt = 0; numberIt < numbers.size(); numberIt++)
+        for (unsigned bitIt = 0; bitIt < colBitCounts.size(); bitIt++)
+            colBitCounts[bitIt] += !!(numbers[numberIt] & (1 << (colBitCounts.size() - bitIt - 1)));
+
     for (unsigned bitIt = 0; bitIt < colBitCounts.size(); bitIt++)
-    {
-        auto bitCount = colBitCounts[bitIt];
-        gamma |= (bitCount > rowCount / 2 ? 1 : 0) << (colBitCounts.size() - bitIt - 1);
-    }
+        gamma |= (colBitCounts[bitIt] > (numbers.size() / 2) ? 1 : 0) << (colBitCounts.size() - bitIt - 1);
 
     epsilon = ~gamma & ((1 << (colBitCounts.size())) - 1);
     
