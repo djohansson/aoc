4871db3ed6ea7d5f403706cd778a1ceb397cce04
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-03 12:38:28 +0100

    better

--- src/2021/day3/day3.cpp
+++ src/2021/day3/day3.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 #include <vector>
 
@@ -14,19 +15,19 @@ int main()
     unsigned gamma = 0, epsilon = 0;
 
     vector<unsigned> numbers;
-    vector<unsigned> colBitCounts;
+    size_t bitWidth = 0;
     string line;
     while (getline(inputFile, line, '\n'))
     {
-        colBitCounts.resize(max(colBitCounts.size(), line.size()));
+        bitWidth = max(bitWidth, line.size());
 
         auto& number = numbers.emplace_back();
         for (unsigned bitIt = 0; bitIt < line.size(); bitIt++)
         {
-            switch (line[bitIt])
+            switch (line[line.size() - bitIt - 1])
             {
             case '1':
-                number |= 1 << (line.size() - bitIt - 1);
+                number |= 1 << bitIt;
                 break;
             default:
                 break;
@@ -34,18 +35,49 @@ int main()
         }
     }
 
-    for (unsigned numberIt = 0; numberIt < numbers.size(); numberIt++)
-        for (unsigned bitIt = 0; bitIt < colBitCounts.size(); bitIt++)
-            colBitCounts[bitIt] += !!(numbers[numberIt] & (1 << (colBitCounts.size() - bitIt - 1)));
+    auto bitCount = [](const auto& numbers, unsigned bit)
+    {
+        unsigned count = 0;
+        for (const auto& number : numbers)
+            count += !!(number & (1 << bit));
 
-    for (unsigned bitIt = 0; bitIt < colBitCounts.size(); bitIt++)
-        gamma |= (colBitCounts[bitIt] > (numbers.size() / 2) ? 1 : 0) << (colBitCounts.size() - bitIt - 1);
+        return count;
+    };
 
-    epsilon = ~gamma & ((1 << (colBitCounts.size())) - 1);
+    for (unsigned bitIt = 0; bitIt < bitWidth; bitIt++)
+    {
+        auto c = bitCount(numbers, bitIt);
+        gamma |= (c > (numbers.size() / 2) ? 1 : 0) << bitIt;
+    }
+
+    epsilon = ~gamma & ((1 << bitWidth) - 1);
     
     cout << gamma << "\n";
     cout << epsilon << "\n";
     cout << gamma * epsilon << "\n";
+
+        // for (unsigned numberIt = 0; numberIt < numbers.size(); numberIt++)
+    //     for (unsigned bitIt = 0; bitIt < bitWidth; bitIt++)
+    //         colBitCounts[bitIt] += !!(numbers[numberIt] & (1 << (colBitCounts.size() - bitIt - 1)));
+
+    // list<unsigned> o2Numbers(numbers.begin(), numbers.end());
+    // vector<unsigned> o2ColBitCounts = colBitCounts;
+    // for (unsigned bitIt = 0; bitIt < colBitCounts.size(); bitIt++)
+    // {
+    //     auto o2NumberIt = o2Numbers.begin();
+    //     while (o2NumberIt != o2Numbers.end())
+    //     {
+    //         if (*o2NumberIt ^ (1 << (colBitCounts.size() - bitIt - 1)))
+    //             o2NumberIt = o2Numbers.erase(o2NumberIt);
+    //         else
+    //             ++o2NumberIt;
+    //     }
+
+    // }
+    
+    
+
+    //vector<unsigned> co2Numbers = numbers;
 
     return 0;
 }
