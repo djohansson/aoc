67b59067e4b12556afe727713b212fd30c6c065c
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-04 22:49:08 +0100

    day17 part2

--- src/2020/day17/day17.cpp
+++ src/2020/day17/day17.cpp
@@ -28,27 +28,29 @@ public:
         return *this;
     }
 
-    void set(int16_t x, int16_t y, int16_t z)
+    void set(int16_t x, int16_t y, int16_t z, int16_t w)
     {
         using namespace std;
         
-        auto [it, result]  = myData.insert(key(x, y, z));
+        auto [it, result]  = myData.insert(key(x, y, z, w));
 
         if (result)
         {
             myMin[0] = min(myMin[0], x);
             myMin[1] = min(myMin[1], y);
             myMin[2] = min(myMin[2], z);
+            myMin[3] = min(myMin[2], w);
 
             myMax[0] = max(myMax[0], x);
             myMax[1] = max(myMax[1], y);
             myMax[2] = max(myMax[2], z);
+            myMax[3] = max(myMax[2], w);
         }
     }
 
-    bool get(int16_t x, int16_t y, int16_t z) const
+    bool get(int16_t x, int16_t y, int16_t z, int16_t w) const
     {
-        if (myData.find(key(x, y, z)) != myData.cend())
+        if (myData.find(key(x, y, z, w)) != myData.cend())
             return true;
         
         return false;
@@ -58,22 +60,24 @@ public:
     {
         uint32_t count = 0;
 
-        for (auto z = myMin[2]; z <= myMax[2]; ++z)
-            for (auto y = myMin[1]; y <= myMax[1]; ++y)
-                for (auto x = myMin[0]; x <= myMax[0]; ++x)
-                    count += get(x, y, z);
+        for (auto w = myMin[3]; w <= myMax[3]; ++w)
+            for (auto z = myMin[2]; z <= myMax[2]; ++z)
+                for (auto y = myMin[1]; y <= myMax[1]; ++y)
+                    for (auto x = myMin[0]; x <= myMax[0]; ++x)
+                        count += get(x, y, z, w);
 
         return count;
     }
 
-    uint8_t neighbourCount(int16_t x, int16_t y, int16_t z) const
+    uint8_t neighbourCount(int16_t x, int16_t y, int16_t z, int16_t w) const
     {
         uint8_t count = 0;
 
-        for (auto zIt = z-1; zIt <= z+1; ++zIt)
-            for (auto yIt = y-1; yIt <= y+1; ++yIt)
-                for (auto xIt = x-1; xIt <= x+1; ++xIt)
-                    count += (zIt == z && yIt == y && xIt == x ? 0 : get(xIt, yIt, zIt));
+        for (auto wIt = w-1; wIt <= w+1; ++wIt)
+            for (auto zIt = z-1; zIt <= z+1; ++zIt)
+                for (auto yIt = y-1; yIt <= y+1; ++yIt)
+                    for (auto xIt = x-1; xIt <= x+1; ++xIt)
+                        count += (wIt == w && zIt == z && yIt == y && xIt == x ? 0 : get(xIt, yIt, zIt, wIt));
 
         return count;
     }
@@ -82,62 +86,60 @@ public:
     {
         Grid next;
 
-        for (auto z = myMin[2]-1; z <= myMax[2]+1; ++z)
-        {
-            for (auto y = myMin[1]-1; y <= myMax[1]+1; ++y)
-            {
-                for (auto x = myMin[0]-1; x <= myMax[0]+1; ++x)
-                {
-                    auto nCount = neighbourCount(x, y, z);
-
-                    if (get(x, y, z))
+        for (auto w = myMin[3]-1; w <= myMax[3]+1; ++w)
+            for (auto z = myMin[2]-1; z <= myMax[2]+1; ++z)
+                for (auto y = myMin[1]-1; y <= myMax[1]+1; ++y)
+                    for (auto x = myMin[0]-1; x <= myMax[0]+1; ++x)
                     {
-                        if (nCount >= 2 && nCount <= 3)
+                        auto nCount = neighbourCount(x, y, z, w);
+
+                        if (get(x, y, z, w))
                         {
-                            next.set(x, y, z);
+                            if (nCount >= 2 && nCount <= 3)
+                                next.set(x, y, z, w);
+                        }
+                        else if (nCount == 3)
+                        {
+                            next.set(x, y, z, w);
                         }
                     }
-                    else if (nCount == 3)
-                    {
-                        next.set(x, y, z);
-                    }
-                }
-            }
-        }
-
+                    
         *this = std::move(next);
     }
 
     void print() const
     {
-        for (auto z = myMin[2]; z <= myMax[2]; ++z)
+        for (auto w = myMin[3]; w <= myMax[3]; ++w)
         {
-            std::cout << "z=" << z << "\n";
-            for (auto y = myMin[1]; y <= myMax[1]; ++y)
+            for (auto z = myMin[2]; z <= myMax[2]; ++z)
             {
-                for (auto x = myMin[0]; x <= myMax[0]; ++x)
-                    std::cout << (get(x, y, z) ? "#" : ".");
-                
+                std::cout << "z=" << z << ", " << "w=" << w << "\n";
+                for (auto y = myMin[1]; y <= myMax[1]; ++y)
+                {
+                    for (auto x = myMin[0]; x <= myMax[0]; ++x)
+                        std::cout << (get(x, y, z, w) ? "#" : ".");
+                    
+                    std::cout << "\n";
+                }
                 std::cout << "\n";
             }
-            std::cout << "\n";
         }
         std::cout << "\n";
     }
 
 private:
-    static uint64_t key(int16_t x, int16_t y, int16_t z)
+    static uint64_t key(int16_t x, int16_t y, int16_t z, int16_t w)
     {
+        uint64_t wKey = (static_cast<uint64_t>(w) + (1 << 15)) << 48;
         uint64_t zKey = (static_cast<uint64_t>(z) + (1 << 15)) << 32;
         uint64_t yKey = (static_cast<uint64_t>(y) + (1 << 15)) << 16;
         uint64_t xKey = static_cast<uint64_t>(x) + (1 << 15);
-        uint64_t key = zKey | yKey | xKey;
-        return key;
+        return wKey | zKey | yKey | xKey;
     }
 
     std::set<uint64_t> myData;
-    std::array<int16_t, 3> myMin = { 0, 0, 0 };
-    std::array<int16_t, 3> myMax = { -1, -1, -1 };
+    std::array<int16_t, 4> myMin = { 0, 0, 0, 0};
+    std::array<int16_t, 4> myMax = { -1, -1, -1, -1 };
 };
 
 int main()
@@ -161,7 +163,7 @@ int main()
                 switch (c)
                 {
                 case '#':
-                    grid.set(x, y, 0);
+                    grid.set(x, y, 0, 0);
                     break;
                 default:
                     break;
@@ -174,14 +176,14 @@ int main()
         }
     }
 
-    grid.print();
+    //grid.print();
 
     for (unsigned cycle = 0; cycle < 6; cycle++)
     {
         cout << "cycle " << cycle << "\n\n";
 
         grid.cycle();
-        grid.print();
+        //grid.print();
     }
 
     cout << grid.count() << "\n";
