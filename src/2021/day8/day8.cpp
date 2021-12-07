a5a5e88badd0c9d3544b54f916242825c1e259bc
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-07 20:00:42 +0100

    day8 stub

---
+++ src/2021/day8/day8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<string>& split(const string& s, char delim, vector<string>& elems)
+{
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim))
+    {
+        if (item.length() > 0)
+            elems.push_back(item);
+    }
+    return elems;
+}
+
+vector<string> split(const string& s, char delim)
+{
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+int main()
+{
+    ifstream inputFile("test.txt");
+    if (!inputFile.is_open())
+        return -1;
+
+    return 0;
+}
