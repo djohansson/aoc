a75a58454d87d1298acf00554b92b6ec60d336ea
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-08 10:37:03 +0100

    day8 part 1

--- src/2021/day8/day8.cpp
+++ src/2021/day8/day8.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <tuple>
 #include <vector>
 
 using namespace std;
@@ -27,9 +28,30 @@ vector<string> split(const string& s, char delim)
 
 int main()
 {
-    ifstream inputFile("test.txt");
+    ifstream inputFile("input.txt");
     if (!inputFile.is_open())
         return -1;
 
+    using SignalPatterns = vector<string>;
+    using OutputValue = vector<string>;
+    using Entry = tuple<SignalPatterns, OutputValue>;
+
+    vector<Entry> entries;
+    string line;
+    while (getline(inputFile, line, '\n'))
+    {
+        auto s = split(line, '|');
+        auto& [sp, o] = entries.emplace_back();
+        sp = split(*s.begin(), ' ');
+        o = split(*next(s.begin()), ' ');
+    }
+
+    unsigned count = 0;
+    for (const auto& [sp, o] : entries)
+        for (const auto& s : o)
+            count += (s.size() == 2 || s.size() == 3 || s.size() == 4 || s.size() == 7);
+
+    cout << count << "\n";
+    
     return 0;
 }
