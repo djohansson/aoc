58a758728e71b34cf2d745cd3eabc8228d1304ef
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-12 21:04:23 +0100

    simpler and faster

--- src/2021/day12/day12.cpp
+++ src/2021/day12/day12.cpp
@@ -1,12 +1,11 @@
-#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <memory>
+#include <set>
 #include <string>
 #include <string_view>
 #include <sstream>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 namespace aoc
@@ -14,8 +13,41 @@ namespace aoc
 
 using namespace std;
 
-enum class CaveType : uint8_t { small, large };
-using CaveMap = map<string, tuple<CaveType, vector<string>>>;
+static const auto sc_hasher = hash<string>{};
+static const auto sc_startHash = sc_hasher("start");
+static const auto sc_endHash = sc_hasher("end");
+
+enum class CaveType { small, large };
+
+struct Cave
+{
+    string name = {};
+    size_t nameHash = {};
+    CaveType type = {};
+    vector<size_t> adjacentNameHashes;
+};
+
+struct CaveCompare
+{
+    using is_transparent = Cave;
+
+    inline bool operator()(const unique_ptr<Cave>& a, const unique_ptr<Cave>& b) const
+    {
+        return a->nameHash < b->nameHash;
+    }
+
+    inline bool operator()(size_t a, const unique_ptr<Cave>& b) const
+    {
+        return a < b->nameHash;
+    }
+
+    inline bool operator()(const unique_ptr<Cave>& a, size_t b) const
+    {
+        return a->nameHash < b;
+    }
+};
+
+using CaveSet = set<unique_ptr<Cave>, CaveCompare>;
 
 static inline string_view toString(CaveType c)
 {
@@ -27,6 +59,59 @@ static inline CaveType getCaveType(const string& s)
     auto it = find_if(s.begin(), s.end(), [](auto ch){ return ::isupper(ch); });
 
     return it == s.end() ? CaveType::small : CaveType::large;
+}
+
+static void print(const CaveSet& caves)
+{
+    for (const auto& cave : caves)
+    {
+        cout << cave->name << ": " << toString(cave->type) << " |";
+
+        for (auto h : cave->adjacentNameHashes)
+            cout << caves.find(h)->get()->name << "|";
+
+        cout << "\n";
+    }
+}
+
+static void traverse(
+    const CaveSet::const_iterator& caveIt,
+    CaveSet& caves,
+    map<size_t, unsigned>& pathMap,
+    bool& twice)
+{
+    if (caveIt == end(caves))
+        return;
+
+    const auto& cave = (*caveIt);
+
+    if (cave->nameHash == sc_endHash)
+    {
+        pathMap[sc_endHash]++;
+        return;
+    }
+
+    bool twiceIsSet = false;
+
+    if (cave->type == CaveType::small && pathMap.find(cave->nameHash) != pathMap.end())
+    {
+        if (twice || cave->nameHash == sc_startHash)
+            return;
+
+        twice = true;
+        twiceIsSet = true;
+    }
+
+    pathMap[cave->nameHash]++;
+    
+    for (auto h : cave->adjacentNameHashes)
+        traverse(caves.find(h), caves, pathMap, twice);
+
+    if (--pathMap[cave->nameHash] == 0)
+        pathMap.erase(cave->nameHash);
+
+    if (twiceIsSet)
+        twice = false;
 }
 
 static vector<string>& split(const string& s, char delim, vector<string>& elems)
@@ -48,55 +133,6 @@ static vector<string> split(const string& s, char delim)
     return elems;
 }
 
-static void print(const CaveMap& caveMap)
-{
-    for (const auto& [key, val] : caveMap)
-    {
-        const auto& [type, adjacent]  = val;
-
-        cout << key << ": " << toString(type) << " |";
-
-        for (const auto& cave : adjacent)
-            cout << cave << "|";
-
-        cout << "\n";
-    }
-}
-
-static void traverse(const string& cave, CaveMap& caveMap, vector<string>& path, vector<vector<string>>& trails, bool& twice)
-{
-    if (cave == "end")
-    {
-        path.emplace_back(cave);
-        trails.emplace_back(path);
-        path.pop_back();
-        return;
-    }
-
-    const auto& [type, adjacent] = caveMap[cave];
-
-    bool twiceSet = false;
-
-    if (type == CaveType::small && find(begin(path), end(path), cave) != end(path))
-    {
-        if (twice || cave == "start")
-            return;
-
-        twice = true;
-        twiceSet = true;
-    }
-
-    path.emplace_back(cave);
-
-    for (const auto& c : adjacent)
-        traverse(c, caveMap, path, trails, twice);
-    
-    path.pop_back();
-
-    if (twiceSet)
-        twice = false;
-}
-
 }
 
 int main()
@@ -107,36 +143,38 @@ int main()
     if (!inputFile.is_open())
         return -1;
 
-    CaveMap caveMap;
+    CaveSet caves;
+
     string line;
     while (getline(inputFile, line, '\n'))
     {
         auto s = split(line, '-');
-        auto& [sfType, sfPaths] = caveMap[s.front()];
-        auto& [sbType, sbPaths] = caveMap[s.back()];
-        sfType = getCaveType(s.front());
-        sbType = getCaveType(s.back());
-        sfPaths.emplace_back(s.back());
-        sbPaths.emplace_back(s.front());
+        
+        auto c1 = make_unique<Cave>();
+        c1->name = s.front();
+        c1->nameHash = hash<string>{}(s.front());
+        c1->type = getCaveType(s.front());
+
+        auto c2 = make_unique<Cave>();
+        c2->name = s.back();
+        c2->nameHash = hash<string>{}(s.back());
+        c2->type = getCaveType(s.back());
+
+        auto [c1It, c1Inserted] = caves.emplace(move(c1));
+        auto [c2It, c2Inserted] = caves.emplace(move(c2));
+
+        (*c1It)->adjacentNameHashes.push_back(c2It->get()->nameHash);
+        (*c2It)->adjacentNameHashes.push_back(c1It->get()->nameHash);
     }
 
-    print(caveMap);
+    print(caves);
 
     bool twice = false;
-    vector<string> path;
-    vector<vector<string>> trails;
-    traverse("start", caveMap, path, trails, twice);
+    map<size_t, unsigned> pathMap;
+    
+    traverse(caves.find(sc_startHash), caves, pathMap, twice);
 
-    for (const auto& trail : trails)
-    {
-        cout << trail.front();
-        for (auto caveIt = next(begin(trail)); caveIt != end(trail); ++caveIt)
-            cout << "," << *caveIt;
-
-        cout << "\n";
-    }
-
-    cout << trails.size() << "\n";
+    cout << pathMap[sc_endHash] << "\n";
 
     return 0;
 }
