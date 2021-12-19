a9e6ee630a7f41921af4617f5aeb8a82de742e30
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-19 12:49:32 +0100

    explode

--- src/2021/day18/day18.cpp
+++ src/2021/day18/day18.cpp
@@ -17,15 +17,10 @@ using namespace std;
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
-struct Pair;
-using Single = std::variant<unsigned, shared_ptr<Pair>>;
+struct Node;
+using Single = std::variant<unsigned, shared_ptr<Node>>;
 using Double = array<Single, 2>;
-struct Pair : public Double
-{
-    //Pair() : Double() {}
-    //Pair(Double&& d, const shared_ptr<Pair>& p) : Double(exchange(d, {})), parent(p) {}
-    weak_ptr<Pair> parent;
-};
+struct Node : public Double { weak_ptr<Node> parent; };
 
 static inline void printDouble(const Double& d);
 
@@ -37,9 +32,10 @@ static inline void printSingle(const Single& s)
         return;
     }
 
-    visit(overloaded {
+    visit(overloaded
+    {
         [](auto arg) { cout << arg; },
-        [](const shared_ptr<Pair>& arg) { printDouble(static_cast<const Double&>(*arg)); }
+        [](const shared_ptr<Node>& arg) { printDouble(*arg); }
     }, s);
 }
 
@@ -52,17 +48,93 @@ static inline void printDouble(const Double& d)
     cout << ']';
 }
 
+static inline void explodeAddUp(Node& node, unsigned value, bool isLeft)
+{
+    if (value == 0)
+        return;
+
+    if (holds_alternative<unsigned>(node[isLeft ? 1 : 0]))
+        get<unsigned>(node[isLeft ? 1 : 0]) += value;
+
+    if (holds_alternative<shared_ptr<Node>>(node[isLeft ? 1 : 0]))
+        explodeAddUp(*get<shared_ptr<Node>>(node[isLeft ? 1 : 0]), value, isLeft);
+}
+
+static inline void explodeAddDown(Node& node, unsigned value, bool isLeft)
+{
+    auto parent = node.parent.lock();
+
+    if (!parent || value == 0)
+        return;
+
+    if (holds_alternative<shared_ptr<Node>>((*parent)[isLeft ? 1 : 0]))
+    {
+        if (&node == get<shared_ptr<Node>>((*parent)[isLeft ? 1 : 0]).get())
+        {
+            if (holds_alternative<unsigned>((*parent)[isLeft ? 0 : 1]))
+                get<unsigned>((*parent)[isLeft ? 0 : 1]) += value;
+            //else
+            //    explodeAddUp(*get<shared_ptr<Node>>((*parent)[isLeft ? 0 : 1]), value, isLeft);
+
+            return;
+        }
+    }
+
+    explodeAddDown(*parent, value, isLeft);
+}
+
+static inline unsigned explode(Single& s, unsigned depth);
+
+static inline bool explode(Node& d, unsigned depth)
+{
+    bool hasExploded = false;
+    for (unsigned i = 0; i < 2; i++)
+    {
+        unsigned val = explode(d[i], depth);
+        hasExploded |= (val > 0);
+        if (hasExploded /*&& holds_alternative<shared_ptr<Node>>(d)*/)
+            explodeAddDown(d, val, i == 0);
+    }
+    return hasExploded;
+}
+
+static inline unsigned explode(Single& s, unsigned depth)
+{
+    assert(s.index() != variant_npos);
+
+    unsigned singleValue = 0;
+    bool doubleHasExploded = false;
+
+    visit(overloaded
+    {
+        [&singleValue, depth](auto arg)
+        {
+            if (depth >= 4)
+                singleValue = arg;
+        },
+        [&doubleHasExploded, depth](shared_ptr<Node>& arg)
+        {
+            doubleHasExploded |= explode((*arg), depth + 1);
+        }
+    }, s);
+
+    if (doubleHasExploded)
+        s = 0u;
+
+    return singleValue;
+}
+
 }
 
 int main()
 {
     using namespace aoc;
 
-    ifstream inputFile("test.txt");
+    ifstream inputFile("test5.txt");
     if (!inputFile.is_open())
         return -1;
 
-    shared_ptr<Pair> root;
+    shared_ptr<Node> root;
     stack<bool> isSecond;
     
     string line;
@@ -79,11 +151,11 @@ int main()
             {
             case '[':
             {
-                auto node = make_shared<Pair>();
-                if (current != root)
+                auto node = make_shared<Node>();
+                if (current/* != root*/)
                 {
                     node->parent = current;
-                    static_cast<Double&>(*current)[isSecond.top()] = node;
+                    (*current)[isSecond.top()] = node;
                     isSecond.push(false);
                     
                 }
@@ -92,11 +164,11 @@ int main()
                     if (root)
                     {
                         auto oldRoot = root;
-                        root = make_shared<Pair>();
+                        root = make_shared<Node>();
                         oldRoot->parent = root;
                         node->parent = root;
-                        static_cast<Double&>(*root)[0] = oldRoot;
-                        static_cast<Double&>(*root)[1] = node;
+                        (*root)[0] = oldRoot;
+                        (*root)[1] = node;
                     }
                     else
                     {
@@ -131,7 +203,7 @@ int main()
             case '8':
             case '9':
             {
-                static_cast<Double&>(*current)[isSecond.top()] = static_cast<unsigned>(c - '0');
+                (*current)[isSecond.top()] = static_cast<unsigned>(c - '0');
                 break;
             }
             default:
@@ -139,7 +211,13 @@ int main()
             }
         }
 
-        printDouble(static_cast<Double&>(*root));
+        printDouble(*root);
+
+        cout << '\n';
+
+        explode(*root, 0);
+
+        printDouble(*root);
 
         cout << '\n';
     }
