281f20d749b869acb48221641e1b52f2d5b17d37
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-20 22:58:20 +0100

    cleanup

--- src/2021/day17/day17.cpp
+++ src/2021/day17/day17.cpp
@@ -26,11 +26,6 @@ constexpr State cx_initial = {0, 0, 0, 0};
 
 static inline void step(State& s)
 {
-    // The probe's x position increases by its x velocity.
-    // The probe's y position increases by its y velocity.
-    // Due to drag, the probe's x velocity changes by 1 toward the value 0; that is, it decreases by 1 if it is greater than 0, increases by 1 if it is less than 0, or does not change if it is already 0.
-    // Due to gravity, the probe's y velocity decreases by 1.
-
     s.px += s.vx;
     s.py += s.vy;
     s.vx = (s.vx > 0 ? s.vx - 1 : (s.vx < 0 ? s.vx + 1 : 0));
