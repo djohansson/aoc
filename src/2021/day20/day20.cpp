3276955edb0c2e22ef0500add2ecd581aedecabc
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-20 20:58:46 +0100

    day20 part 1 & 2

--- src/2021/day20/day20.cpp
+++ src/2021/day20/day20.cpp
@@ -1,131 +1,131 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <set>
+#include <string>
 
 namespace aoc
 {
 
 using namespace std;
 
-
 class Grid
 {
 public:
 
-    Grid() = default;
+    Grid(const string& algorithm, bool infiniteValue = false)
+     : myAlgorithm(algorithm)
+     , myInfiniteValue(infiniteValue)
+    {}
     Grid(const Grid& other) = delete;
     Grid(Grid&& other) noexcept
     : myData(exchange(other.myData, {}))
+    , myAlgorithm(exchange(other.myAlgorithm, {}))
     , myMin(exchange(other.myMin, {}))
-    , myMax(exchange(other.myMax, {})) {}
+    , myMax(exchange(other.myMax, {}))
+    , myInfiniteValue(exchange(other.myInfiniteValue, {}))
+     {}
 
     Grid& operator=(Grid&& other) noexcept
     {
         if (this != &other)
         {
             myData = exchange(other.myData, {});
+            myAlgorithm = exchange(other.myAlgorithm, {});
             myMin = exchange(other.myMin, {});
             myMax = exchange(other.myMax, {});
+            myInfiniteValue = exchange(other.myInfiniteValue, {});
         }
 
         return *this;
     }
 
-    void set(int32_t x, int32_t y)
+    void put(int16_t x, int16_t y)
     {
-        using namespace std;
-        
         auto [it, result]  = myData.insert(key(x, y));
 
         if (result)
         {
-            myMin[0] = min(myMin[0], x);
-            myMin[1] = min(myMin[1], y);
-            myMax[0] = max(myMax[0], x);
-            myMax[1] = max(myMax[1], y);
+            myMin[0] = min<int16_t>(myMin[0], x);
+            myMin[1] = min<int16_t>(myMin[1], y);
+            myMax[0] = max<int16_t>(myMax[0], x);
+            myMax[1] = max<int16_t>(myMax[1], y);
         }
     }
 
-    bool get(int32_t x, int32_t y) const
+    bool get(int16_t x, int16_t y) const
     {
+        if (x < myMin[0] || x > myMax[0] || y < myMin[1] || y > myMax[1])
+            return myInfiniteValue;
+
         if (myData.find(key(x, y)) != myData.cend())
             return true;
         
         return false;
     }
 
-    uint64_t count() const
+    uint32_t count() const
     {
-        uint64_t count = 0;
+        uint32_t count = 0;
 
-        for (auto y = myMin[1]; y <= myMax[1]; ++y)
-            for (auto x = myMin[0]; x <= myMax[0]; ++x)
-                count += get(x, y);
+        for (auto yIt = myMin[1]; yIt <= myMax[1]; ++yIt)
+            for (auto xIt = myMin[0]; xIt <= myMax[0]; ++xIt)
+                count += get(xIt, yIt);
 
         return count;
     }
 
-    // uint8_t neighbourCount(int16_t x, int16_t y) const
-    // {
-    //     uint8_t count = 0;
-
-    //     for (auto yIt = y-1; yIt <= y+1; ++yIt)
-    //         for (auto xIt = x-1; xIt <= x+1; ++xIt)
-    //             count += (yIt == y && xIt == x ? 0 : get(xIt, yIt));
-
-    //     return count;
-    // }
-
-    uint16_t code(int32_t x, int32_t y) const
+    uint16_t code(int16_t x, int16_t y) const
     {
-        uint8_t count = 8;
+        uint8_t count = 9;
         uint16_t code = 0;
 
-        for (auto yIt = y-1; yIt <= y+1; ++yIt)
-            for (auto xIt = x-1; xIt <= x+1; ++xIt, --count)
-                code |= get(xIt, yIt) << count;
+        for (int16_t yIt = y-1; yIt <= y+1; ++yIt)
+            for (int16_t xIt = x-1; xIt <= x+1; ++xIt)
+                code |= get(xIt, yIt) << --count;
 
         return code;
     }
 
-    void cycle(const string& algorithm)
+    void cycle()
     {
-        Grid next;
+        Grid next(myAlgorithm, myAlgorithm[myInfiniteValue ? 0b111111111 : 0] == '#');
 
-        for (auto y = myMin[1]-1; y <= myMax[1]+1; ++y)
-            for (auto x = myMin[0]-1; x <= myMax[0]+1; ++x)
-                if (algorithm[code(x, y)] == '#')
-                    next.set(x, y);
-                    
+        for (auto yIt = myMin[1]-1; yIt <= myMax[1]+1; ++yIt)
+            for (auto xIt = myMin[0]-1; xIt <= myMax[0]+1; ++xIt)
+                if (myAlgorithm[code(xIt, yIt)] == '#')
+                    next.put(xIt, yIt);
+
         *this = move(next);
     }
 
     void print() const
     {
-        for (auto y = myMin[1]; y <= myMax[1]; ++y)
-        {
-            for (auto x = myMin[0]; x <= myMax[0]; ++x)
-                cout << (get(x, y) ? '#' : '.');
-            
-            cout << '\n';
-        }
-        cout << '\n';
+        cout << "\nSize: (" << myMax[0] - myMin[0] << ',' << myMax[1] - myMin[1] << ')';
+        
+        // for (auto y = myMin[1] - 3; y <= myMax[1] + 3; ++y, cout << '\n')
+        //     for (auto x = myMin[0] - 3; x <= myMax[0] + 3; ++x)
+        //         cout << (get(x, y) ? '#' : '.');
+
+        cout << "\nCount: " << count();
     }
 
 private:
-    static uint64_t key(int32_t x, int32_t y)
+
+    static uint32_t key(int16_t x, int16_t y)
     {
-        uint64_t yKey = (static_cast<uint64_t>(y) + (1 << 15)) << 16;
-        uint64_t xKey = static_cast<uint64_t>(x) + (1 << 15);
+        uint32_t yKey = (static_cast<uint32_t>(y) + (1 << 15)) << 16;
+        uint32_t xKey = static_cast<uint32_t>(x) + (1 << 15);
         return yKey | xKey;
     }
 
-    std::set<uint64_t> myData;
-    array<int32_t, 2> myMin = { 0, 0 };
-    array<int32_t, 2> myMax = { -1, -1 };
+    set<uint32_t> myData;
+    string myAlgorithm;
+    array<int16_t, 2> myMin = { 0, 0 };
+    array<int16_t, 2> myMax = { -1, -1 };
+    bool myInfiniteValue = false;
 };
 
 }
@@ -138,12 +138,14 @@ int main()
     if (!inputFile.is_open())
         return -1;
 
-    Grid image;
+    unique_ptr<Grid> image;
     string algorithm;
     string line;
     while (getline(inputFile, line, '\n'))
     {
         algorithm = line;
+
+        image = make_unique<Grid>(algorithm);
 
         if (inputFile.peek() == '\n')
         {
@@ -157,7 +159,7 @@ int main()
                 for (auto c : line2)
                 {
                     if (c == '#')
-                        image.set(x, y);
+                        image->put(x, y);
 
                     x++;
                 }
@@ -168,13 +170,13 @@ int main()
         }
     }
 
-    image.print();
-    image.cycle(algorithm);
-    //image.print();
-    image.cycle(algorithm);
-    //image.print();
+    image->print();
 
-    cout << image.count();
+    for (unsigned i = 0; i < 50; i++)
+    {
+        image->cycle();
+        image->print();
+    }
 
     return 0;
 }
