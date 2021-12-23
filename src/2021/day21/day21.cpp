3a6711a8145c4125a77140b1ae0d6ca38fa1fcea
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-23 10:53:42 +0100

    day21 part 1

--- src/2021/day21/day21.cpp
+++ src/2021/day21/day21.cpp
@@ -1,11 +1,18 @@
-#include <fstream>
+#include <algorithm>
 #include <iostream>
-#include <string>
 
 namespace aoc
 {
 
 using namespace std;
+
+template <typename T>
+constexpr auto sizeof_array(const T& iarray)
+{
+    return (sizeof(iarray) / sizeof(iarray[0]));
+}
+
+constexpr unsigned cx_starts[] = { 8, 6 };
 
 }
 
@@ -13,14 +20,39 @@ int main()
 {
     using namespace aoc;
 
-    ifstream inputFile("input.txt");
-    if (!inputFile.is_open())
-        return -1;
+    unsigned scores[] = { 0, 0 };
+    unsigned positions[] = { 0, 0 };
+    for (auto i = 0; i < sizeof_array(positions); i++)
+        positions[i] = cx_starts[i] - 1; // 0-based position
 
-    string line;
-    while (getline(inputFile, line, '\n'))
+    unsigned count = 0;
+    unsigned die = 0;
+
+    while (true)
     {
+        for (auto i = 0; i < sizeof_array(scores); i++)
+        {
+            for (auto j = 0; j < 3; j++)
+            {
+                count++;
+                die = (die % 100) + 1;
+                positions[i] += die;
+                positions[i] %= 10;
+            }
+
+            scores[i] += (positions[i] + 1);
+            cout << scores[i] << '\n';
+
+            if (scores[i] >= 1000)
+                goto end;
+        }
     }
+
+end:
+
+    auto minScore = min_element(begin(scores), end(scores));
+
+    cout << count * *minScore;
 
     return 0;
 }
