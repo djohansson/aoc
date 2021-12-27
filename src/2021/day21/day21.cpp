b7246c6e134c8b03180ead0e26e149c9385aaa07
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-27 15:20:26 +0100

    iteration 2

--- src/2021/day21/day21.cpp
+++ src/2021/day21/day21.cpp
@@ -14,35 +14,15 @@ constexpr auto sizeof_array(const T& iarray)
 }
 
 constexpr unsigned cx_starts[] = { 8, 6 };
-constexpr unsigned cx_outcomes[][3] =
+constexpr unsigned cx_outcomes[][2] =
 {
-    { 1, 1, 1 },
-    { 1, 1, 2 },
-    { 1, 1, 3 },
-    { 1, 2, 1 },
-    { 1, 2, 2 },
-    { 1, 2, 3 },
-    { 1, 3, 1 },
-    { 1, 3, 2 },
-    { 1, 3, 3 },
-    { 2, 1, 1 },
-    { 2, 1, 2 },
-    { 2, 1, 3 },
-    { 2, 2, 1 },
-    { 2, 2, 2 },
-    { 2, 2, 3 },
-    { 2, 3, 1 },
-    { 2, 3, 2 },
-    { 2, 3, 3 },
-    { 3, 1, 1 },
-    { 3, 1, 2 },
-    { 3, 1, 3 },
-    { 3, 2, 1 },
-    { 3, 2, 2 },
-    { 3, 2, 3 },
-    { 3, 3, 1 },
-    { 3, 3, 2 },
-    { 3, 3, 3 }
+    { 3, 1 }, //{ 1, 1, 1 },
+    { 4, 3 }, //{ 1, 1, 2 },{ 1, 2, 1 },{ 2, 1, 1 },
+    { 5, 6 }, //{ 1, 1, 3 },{ 1, 2, 2 },{ 1, 3, 1 },{ 2, 1, 2 },{ 2, 2, 1 },{ 3, 1, 1 },
+    { 6, 7 }, //{ 1, 2, 3 },{ 1, 3, 2 },{ 2, 1, 3 },{ 2, 2, 2 },{ 2, 3, 1 },{ 3, 1, 2 },{ 3, 2, 1 },
+    { 7, 6 }, //{ 1, 3, 3 },{ 2, 2, 3 },{ 2, 3, 2 },{ 3, 1, 3 },{ 3, 2, 2 },{ 3, 3, 1 },
+    { 8, 3 }, //{ 2, 3, 3 },{ 3, 2, 3 },{ 3, 3, 2 },
+    { 9, 1 }  //{ 3, 3, 3 }
 };
 
 struct Player
@@ -63,7 +43,7 @@ int main()
         ps.push(Player{i, uint16_t(cx_starts[i] - 1), 0});
 
     uint64_t count = 0;
-    uint8_t die = 0;
+    //uint8_t die = 0;
     uint64_t wins[2] = { 0ull, 0ull };
 
     while (!ps.empty())
@@ -74,22 +54,26 @@ int main()
         for (auto outcome = 0; outcome < sizeof(cx_outcomes); outcome++)
         {
             auto pq = p;
-            for (auto j = 0; j < 3; j++)
-            {
-                count++;
-                //die = (die % 100) + 1;
-                die = cx_outcomes[outcome][j];
-                pq.position += die;
-                pq.position %= 10;
-            }
+            // for (auto j = 0; j < 3; j++)
+            // {
+            //     count++;
+            //     //die = (die % 100) + 1;
+            //     die = cx_outcomes[outcome][j];
+            //     pq.position += die;
+            //     pq.position %= 10;
+            // }
+            count += 3;
+            pq.position += cx_outcomes[outcome][0];
+            pq.position %= 10;
             pq.score += (pq.position + 1);
             if (pq.score >= 21)
-                wins[pq.player]++;
+                wins[pq.player] += cx_outcomes[outcome][1];
             else
                 ps.push(pq);
         }
 
-        cout << count << '\n';
+        if (count % 300000 == 0)
+            cout << count << '\n';
     }
 
     cout << wins[0] << '\n';
