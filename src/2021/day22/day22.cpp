197e3594db252ed6fa7140a6996bb58bf942a89d
Daniel Johansson <1911831+djohansson@users.noreply.github.com>
2021-12-29 19:44:36 +0100

    overengineering part 1

--- src/2021/day22/day22.cpp
+++ src/2021/day22/day22.cpp
@@ -74,158 +74,292 @@ template<typename T, uint8_t N>
 using Coord = array<T, N>;
 
 template<typename T, uint8_t N>
-using Bounds = tuple<Coord<T, N>, Coord<T, N>>;
+static constexpr Coord<T, N> cx_lowestCoord = make_array_n<N>(numeric_limits<T>::lowest());
 
 template<typename T, uint8_t N>
-static constexpr Coord<T, N> cx_invalidCoord = make_array_n<N>(numeric_limits<T>::min());
+static constexpr Coord<T, N> cx_minCoord = make_array_n<N>(numeric_limits<T>::min());
 
 template<typename T, uint8_t N>
-class Grid
+static constexpr Coord<T, N> cx_maxCoord = make_array_n<N>(numeric_limits<T>::max());
+
+template <typename T, uint8_t N>
+class Bounds
+{
+public:
+
+    static constexpr size_t cx_cornerCount = 1 << N;
+
+    using value_type = tuple<Coord<T, N>, Coord<T, N>>;
+	
+    constexpr Bounds() noexcept
+		: myBounds{cx_maxCoord<T, N>, cx_lowestCoord<T, N>}
+    { }
+
+    constexpr Bounds(const Bounds<T, N>& other) noexcept
+		: myBounds{other.myBounds}
+	{ }
+    
+    template <typename U = T>
+	constexpr Bounds(const Bounds<U, N>& other) noexcept
+		: myBounds{static_cast<value_type>(other.myBounds)}
+	{ }
+
+    constexpr Bounds(const Coord<T, N>& min, const Coord<T, N>& max) noexcept
+		: myBounds{min, max}
+    { }
+
+    constexpr Bounds(Coord<T, N>&& min, Coord<T, N>&& max) noexcept
+		: myBounds{forward<Coord<T, N>>(min), forward<Coord<T, N>>(max)}
+    { }
+
+    Bounds& operator=(const Bounds& other)
+    {
+        if (this != &other)
+            myBounds = other.myBounds;
+
+        return *this;
+    }
+
+    Bounds& operator=(Bounds&& other)
+    {
+        if (this != &other)
+            myBounds = forward(other.myBounds);
+        
+        return *this;
+    }
+
+	operator bool() const
+	{
+		return (max() < min()) == 0;
+	}
+
+	bool operator!() const
+	{
+		return !static_cast<bool>(*this);
+	}
+
+    auto& min() { return get<0>(myBounds); };
+    const auto& min() const { return get<0>(myBounds); };
+
+    auto& max() { return get<1>(myBounds); };
+    const auto& max() const { return get<1>(myBounds); };
+
+    auto& bounds() { return myBounds; };
+    const auto& bounds() const { return myBounds; };
+
+private:
+
+    value_type myBounds;
+};
+
+template<typename T, uint8_t N>
+static constexpr Bounds<T, N> cx_invalidBounds = {};
+
+template<typename T, uint8_t N>
+class BoundsIterator
+{
+public:
+
+    using iterator_category = forward_iterator_tag;
+    using value_type = Coord<T, N>;
+    using difference_type = value_type;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+
+    constexpr BoundsIterator() noexcept
+    : myBounds(cx_invalidBounds<T, N>)
+    , myPos(cx_lowestCoord<T, N>)
+    { }
+
+    constexpr BoundsIterator(const Bounds<T, N>& bounds) noexcept
+    : myBounds(bounds)
+    , myPos(myBounds.min())
+    { }
+
+    constexpr BoundsIterator(Bounds<T, N>&& bounds) noexcept
+    : myBounds(forward(bounds))
+    , myPos(myBounds.min())
+    { }
+
+    constexpr BoundsIterator(const Bounds<T, N>& bounds, const Coord<T, N>& start) noexcept
+    : myBounds(bounds)
+    , myPos(start)
+    { }
+
+    constexpr BoundsIterator(Bounds<T, N>&& bounds, Coord<T, N>&& start) noexcept
+    : myBounds(forward(bounds))
+    , myPos(forward(start))
+    { }
+
+    constexpr BoundsIterator(const BoundsIterator& other) noexcept
+    : myBounds(other.bounds)
+    , myPos(myBounds.min())
+    { }
+    
+    constexpr BoundsIterator(BoundsIterator&& other) noexcept
+    : myBounds(forward(other.bounds))
+    , myPos(myBounds.min())
+    { }
+
+    BoundsIterator& operator=(const BoundsIterator& other)
+    {
+        if (this != &other)
+        {
+            myBounds = other.bounds;
+            myPos = myBounds.min();
+        }
+
+        return *this;
+    }
+
+    BoundsIterator& operator=(BoundsIterator&& other)
+    {
+        if (this != &other)
+        {
+            myBounds = forward(other.bounds);
+            myPos = myBounds.min();
+        }
+
+        return *this;
+    }
+
+    operator bool() const
+	{
+		return valid();
+	}
+
+	bool operator!() const
+	{
+		return !static_cast<bool>(*this);
+	}
+
+    bool operator==(const BoundsIterator& other) const
+    {
+        return myBounds == other.myBounds && myPos == other.myPos;
+    }
+
+    bool operator!=(const BoundsIterator& other) const
+    {
+        return !(*this == other);
+    }
+
+    reference& operator*()
+    {
+        return myPos;
+    }
+
+    const_reference& operator*() const
+    {
+        return myPos;
+    }
+
+    pointer* operator->()
+    {
+        return &myPos;
+    }
+
+    const_pointer* operator->() const
+    {
+        return &myPos;
+    }
+
+    BoundsIterator& operator++()
+    {
+        if (!valid())
+        {
+            assert(false);
+            return *this;
+        }
+
+        const auto& [bmin, bmax] = myBounds.bounds();
+        if (bmin == bmax)
+            return *this;
+
+        if (myPos == bmax)
+        {
+            myBounds = cx_invalidBounds<T, N>;
+            myPos = cx_lowestCoord<T, N>;
+            return *this;
+        }
+
+        size_t d = 0;
+        for (; d < myPos.size(); d++)
+        {
+            auto range = bmax[d] - bmin[d] + 1;
+            auto shiftToZero = myPos[d] - bmin[d];
+            auto next = ++shiftToZero;
+            auto rest = next % range;
+            auto shiftBack = rest + bmin[d];
+
+            myPos[d] = shiftBack;
+
+            if (next >= range)
+                continue;
+
+            break;
+        }
+
+        return *this;
+    }
+
+    bool valid() const 
+    {
+        const auto& [bmin, bmax] = myBounds.bounds();
+        if (myPos > bmax || myPos < bmin)
+            return false;
+
+        return true;
+    }
+
+private:
+
+    Bounds<T, N> myBounds;
+    Coord<T, N> myPos;
+};
+
+
+template<typename T, uint8_t N>
+class Grid : public Bounds<T, N>
 {
 public:
 
     enum class Operation { Fill, Clear };
     using Command = tuple<Operation, Bounds<T, N>>;
-    class Iterator
-    {
-    public:
+
+    Grid()
+    : Bounds<T, N>(cx_minCoord<T, N>, cx_maxCoord<T, N>)
+    { }
     
-        using iterator_category = forward_iterator_tag;
-        using value_type = Coord<T, N>;
-        using difference_type = value_type;
-        using pointer = value_type*;
-        using const_pointer = const value_type*;
-        using reference = value_type&;
-        using const_reference = const value_type&;
-
-        Iterator(const Bounds<T, N>& bounds)
-        : myBounds(bounds)
-        , myPos(std::get<0>(myBounds))
-        { }
-
-        Iterator(const Bounds<T, N>& bounds, const Coord<T, N>& start)
-        : myBounds(bounds)
-        , myPos(start)
-        { }
-
-        Iterator(const Iterator& other)
-        : myBounds(other.bounds)
-        , myPos(std::get<0>(myBounds))
-        { }
-        
-        Iterator(Iterator&& other)
-        : myBounds(exchange(other.bounds, {}))
-        , myPos(std::get<0>(myBounds))
-        { }
-
-        Iterator& operator=(const Iterator& other)
-        {
-            myBounds = other.bounds;
-            myPos = std::get<0>(myBounds);
-            return *this;
-        }
-
-        Iterator& operator=(Iterator&& other)
-        {
-            myBounds = exchange(other.bounds, {});
-            myPos = std::get<0>(myBounds);
-            return *this;
-        }
-
-        bool operator==(const Iterator& other) const
-        {
-            return myBounds == other.myBounds && myPos == other.myPos;
-        }
-
-        bool operator!=(const Iterator& other) const
-        {
-            return !(*this == other);
-        }
-
-        reference& operator*()
-        {
-            return myPos;
-        }
-
-        const_reference& operator*() const
-        {
-            return myPos;
-        }
-
-        pointer* operator->()
-        {
-            return &myPos;
-        }
-
-        const_pointer* operator->() const
-        {
-            return &myPos;
-        }
-
-        Iterator& operator++()
-        {
-            if (!valid())
-            {
-                assert(false);
-                return *this;
-            }
-
-            const auto& [bmin, bmax] = myBounds;
-            if (bmin == bmax)
-                return *this;
-
-            if (myPos == bmax)
-            {
-                myPos = cx_invalidCoord<T, N>;
-                return *this;
-            }
-
-            size_t d = 0;
-            for (; d < myPos.size(); d++)
-            {
-                auto range = bmax[d] - bmin[d] + 1;
-                auto shiftToZero = myPos[d] - bmin[d];
-                auto next = ++shiftToZero;
-                auto rest = next % range;
-                auto shiftBack = rest + bmin[d];
-
-                myPos[d] = shiftBack;
-
-                if (next >= range)
-                    continue;
-
-                break;
-            }
-
-            return *this;
-        }
-
-        bool valid() const 
-        {
-            const auto& [bmin, bmax] = myBounds;
-            if (myPos > bmax || myPos < bmin)
-                return false;
-
-            return true;
-        }
-
-    private:
-
-        const Bounds<T, N>& myBounds;
-        Coord<T, N> myPos;
-    };
-
-    Grid() = default;
-    Grid(const Grid& other) = delete;
+    Grid(const Grid& other)
+    : Bounds<T, N>(other)
+    , myData(other.data)
+    { }
     
     Grid(Grid&& other) noexcept
-    : myData(exchange(other.myData, {}))
+    : Bounds<T, N>(forward(other))
+    , myData(exchange(other.myData, {}))
     { }
+
+    Grid& operator=(const Grid& other) noexcept
+    {
+        if (this != &other)
+        {
+            static_cast<Bounds<T, N>>(*this) = other;
+            myData = other.myData;
+        }
+
+        return *this;
+    }
 
     Grid& operator=(Grid&& other) noexcept
     {
         if (this != &other)
+        {
+            static_cast<Bounds<T, N>>(*this) = other;
             myData = exchange(other.myData, {});
+        }
 
         return *this;
     }
@@ -235,7 +369,7 @@ public:
     void put(const Coord<T, N>& coord) { myData.emplace(coord); }
     void put(const Bounds<T, N>& bounds)
     {
-        Iterator bIt(bounds);
+        BoundsIterator<T, N> bIt(bounds);
         while (bIt.valid())
         {
             put(*bIt);
@@ -248,7 +382,7 @@ public:
     void clear(const Coord<T, N>& coord) { myData.erase(coord); }
     void clear(const Bounds<T, N>& bounds)
     {
-        Iterator bIt(bounds);
+        BoundsIterator<T, N> bIt(bounds);
         while (bIt.valid())
         {
             clear(*bIt);
@@ -279,7 +413,7 @@ public:
     auto size(const Bounds<T, N>& bounds)
     {
         size_t result = 0;
-        Iterator bIt(bounds);
+        BoundsIterator<T, N> bIt(bounds);
         while (bIt.valid())
         {
             result += get(*bIt);
@@ -350,7 +484,7 @@ int main()
     while (getline(inputFile, line, '\n'))
     {
         auto& [op, bounds] = commands.emplace_back();
-        auto& [bmin, bmax] = bounds;
+        auto& [bmin, bmax] = bounds.bounds();
         auto commandStr = split(line, ' ');
         if (commandStr.front() == "on")
             op = Grid3i32::Operation::Fill;
@@ -370,8 +504,8 @@ int main()
 
     for (const auto& command : commands)
     {
-        auto& [op, bounds] = command;
-        auto& [bmin, bmax] = bounds;
+        const auto& [op, bounds] = command;
+        const auto& [bmin, bmax] = bounds.bounds();
         if (any_of(begin(bmin), end(bmin), [](auto n) { return n < -50; }) ||
             any_of(begin(bmax), end(bmax), [](auto n) { return n > 50; }))
             continue;
